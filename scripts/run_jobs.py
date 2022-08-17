7c1aae12d426c2c98504dfcd35598dec033a3971
Adrian Rudloff <80639001+a-rud@users.noreply.github.com>
2022-08-17 21:23:16 +0200

    Update Dockerfile to include sysstat for CPU monitoring

--- scripts/run_jobs.py
+++ scripts/run_jobs.py
@@ -2,35 +2,49 @@
 Run multiple experiments on a single machine.
 """
 import subprocess
+import time
 
 import numpy as np
 
-ALGOS = ["sac"]
-ENVS = ["MountainCarContinuous-v0"]
-N_SEEDS = 10
+ALGOS = ["tqc"]
+ENVS = ["PandaReach-v2"]
+LOG_DIR = "/home/adrian/Downloads/testlogs/"
+SEEDS = [2, 3, 4]
 EVAL_FREQ = 5000
-N_EVAL_EPISODES = 10
-LOG_STD_INIT = [-6, -5, -4, -3, -2, -1, 0, 1]
+N_EVAL_EPISODES = 100
+N_TIMESTEPS = 2000  # int(1e6)
 
 for algo in ALGOS:
     for env_id in ENVS:
-        for log_std_init in LOG_STD_INIT:
-            log_folder = f"logs_std_{np.exp(log_std_init):.4f}"
-            for _ in range(N_SEEDS):
-                args = [
-                    "--algo",
-                    algo,
-                    "--env",
-                    env_id,
-                    "--hyperparams",
-                    f"policy_kwargs:dict(log_std_init={log_std_init}, net_arch=[64, 64])",
-                    "--eval-episodes",
-                    N_EVAL_EPISODES,
-                    "--eval-freq",
-                    EVAL_FREQ,
-                    "-f",
-                    log_folder,
-                ]
-                args = list(map(str, args))
+        for seed in SEEDS:
+            args = [
+                "--algo",
+                algo,
+                "--env",
+                env_id,
+                "--seed",
+                seed,
+                "--eval-episodes",
+                N_EVAL_EPISODES,
+                "--eval-freq",
+                EVAL_FREQ,
+                # "--device \"cuda\"",
+                "--n-timesteps",
+                N_TIMESTEPS,
+                # "--hyperparams",
+                # f"n_envs:{N_ENVS} gradient_steps:{N_ENVS}",
+                # "--num-threads",
+                # N_ENVS,
+                "--log-folder",
+                LOG_DIR,
+                "--verbose",
+                0
+            ]
+            args = list(map(str, args))
 
-                ok = subprocess.call(["python", "train.py"] + args)
+            print(100*"+")
+            print(100*"+")
+            ok = subprocess.call(["python", "train.py"] + args)
+            print(100*"-")
+            print(100*"-")
+            #time.sleep(1.0)
