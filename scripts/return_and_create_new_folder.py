92743eed484e02a209e0b0e56fe2aa1d0045e06a
Adrian Rudloff <80639001+a-rud@users.noreply.github.com>
2022-09-20 01:55:59 +0200

    Add script to create a new folder for a set of experiments

---
+++ scripts/return_and_create_new_folder.py
@@ -0,0 +1,13 @@
+import argparse
+import sys
+
+from synergy_curriculum.utils import get_and_create_save_folder
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--log-path", help="Path of logs to look for runs", type=str, required=True)
+    parser.add_argument("-e", "--env", help="Name of environment.", type=str, required=True)
+    args = parser.parse_args()
+
+    folder_name = get_and_create_save_folder(directory=args.log_path, prefix=args.env)
+    sys.stdout.write(f"{folder_name}")
