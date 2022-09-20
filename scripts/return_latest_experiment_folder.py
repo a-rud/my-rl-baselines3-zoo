1268bc7ca9c612e7011b75648110bc249e755993
Adrian Rudloff <80639001+a-rud@users.noreply.github.com>
2022-09-20 22:31:36 +0200

    Fix return_latest_experiment_folder.py

--- scripts/return_latest_experiment_folder.py
+++ scripts/return_latest_experiment_folder.py
@@ -1,10 +1,11 @@
+from typing import Tuple
 import argparse
 import sys
 import glob
 import os.path
 
 
-def get_latest_experiment_folder(log_path: str, env_name: str) -> int:
+def get_latest_experiment_folder(log_path: str, env_name: str) -> Tuple[str, str]:
     """
     Same function as in utils/utils.py but it doesn't throw gym warnings.
 
@@ -18,7 +19,9 @@ def get_latest_experiment_folder(log_path: str, env_name: str) -> int:
     all_paths = glob.glob(os.path.join(log_path, env_name + "_[0-9]*"))
     assert len(all_paths) > 0, f"No valid folders were found at {log_path}/{env_name}"
     sorted_paths = sorted(all_paths, reverse=True)  # sort in descending order
-    return sorted_paths[0]
+    exp_path = sorted_paths[0]
+    exp_folder = os.path.basename(os.path.normpath(exp_path))
+    return exp_path, exp_folder
 
 
 if __name__ == "__main__":
@@ -27,7 +30,7 @@ if __name__ == "__main__":
     parser.add_argument("-e", "--env", help="Name of environment. Must NOT include version!", type=str, required=True)
     args = parser.parse_args()
 
-    latest_experiment_folder = get_latest_experiment_folder(args.log_path, args.env)
+    _, latest_experiment_folder = get_latest_experiment_folder(args.log_path, args.env)
     sys.stdout.write(f"{latest_experiment_folder}")
 
     # latest_run_name = f"{args.env}_{latest_run_id}"
